d4fab3c250f47369e6b0e78a583e7dd2797a4694
RagtimeRebel <RagtimeRebel@users.noreply.github.com>
2023-01-08 00:32:39 +0000

    Install Racket before container opens

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,8 @@
+cd /workspaces/racketeer/
+
 sudo apt update
 sudo yes Y | apt-get install software-properties-common
 sudo yes Y | add-apt-repository ppa:plt/racket
 sudo yes Y | sudo apt-get install racket
+
+racket -t success.rkt
---
+++ success.rkt
@@ -0,0 +1,4 @@
+#lang racket
+
+(define success "Racket environment installed successfully.")
+(println success)
