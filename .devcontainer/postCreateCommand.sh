b522fb954945721fd182f63b1e6ca6a2afbeee52
RagtimeRebel <RagtimeRebel@users.noreply.github.com>
2023-01-08 00:34:58 +0000

    Removed racket command from build sequence

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,5 +4,3 @@ sudo apt update
 sudo yes Y | apt-get install software-properties-common
 sudo yes Y | add-apt-repository ppa:plt/racket
 sudo yes Y | sudo apt-get install racket
-
-racket -t success.rkt
