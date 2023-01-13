1c5fdb1b46e6486466739795c790a801bb85e352
RagtimeRebel <RagtimeRebel@users.noreply.github.com>
2023-01-12 16:22:55 -0800

    Install Racket v8.7 (latest)

--- .devcontainer/updateContent.sh
+++ .devcontainer/updateContent.sh
@@ -1,15 +1,30 @@
-sudo apt update
-sudo yes Y | apt-get install software-properties-common
+sudo apt-get update
+sudo apt-get upgrade
 
-# sudo yes Y | apt-get install x11-xserver-utils
+yes Y | apt-get install software-properties-common
+
+# Commented out because it downloads version 7.9 instead of 8.7 (latest)
+# yes Y | sudo add-apt-repository ppa:plt/racket
+# yes Y | sudo apt install racket
+
+# Commented out because snap refuses to work on either Ubuntu or Debian
+# yes Y | sudo apt install snapd
+# sudo systemctl unmask snapd.service
+# sudo systemctl enable snapd.service
+# sudo systemctl start snapd.service
+# yes Y | sudo snap install racket
+
+yes Y | sudo add-apt-repository ppa:plt/racket
+sudo wget "https://download.racket-lang.org/releases/8.7/installers/racket-8.7-x86_64-linux-cs.sh"
+sudo chmod +x racket-8.7-x86_64-linux-cs.sh
+printf 'no\n2\n/usr/local\n' | sudo ./racket-8.7-x86_64-linux-cs.sh
+
+# Commented out because this does not fix the pesky error: Gtk initialization failed for display ":0"
+# sudo yes Y | apt install x11-xserver-utils
 # sudo xhost +
+# sudo yes Y | apt install --no-install-recommends libjpeg62-dev
+# sudo yes Y | apt install --no-install-recommends libgtk2.0-dev
+# sudo yes Y | apt install --no-install-recommends xvfb
 
-sudo yes Y | add-apt-repository ppa:plt/racket
-sudo yes Y | sudo apt-get install racket
-
-# sudo yes Y | apt-get install --no-install-recommends libjpeg62-dev
-# sudo yes Y | apt-get install --no-install-recommends libgtk2.0-dev
-# sudo yes Y | apt-get install --no-install-recommends xvfb
-
-sudo yes Y | raco pkg install racket-langserver
-sudo yes Y | raco pkg install pollen
+yes Y | raco pkg install racket-langserver
+yes Y | raco pkg install pollen
